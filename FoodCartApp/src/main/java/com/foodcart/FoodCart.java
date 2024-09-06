e680af90cf068da3b339a47cf46b30c65d8d62a4
swapnilk30 <sk9137587676@gmail.com>
2024-09-06 23:17:07 +0530

    .

--- FoodCartApp/src/main/java/com/foodcart/FoodCart.java
+++ FoodCartApp/src/main/java/com/foodcart/FoodCart.java
@@ -1,6 +1,8 @@
 package com.foodcart;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +15,17 @@ public class FoodCart extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
+		//Get Data from the database(model)
+		
+		String[] foodItems = {"Biryani","Pizza","Lollypop"};
+		
+		request.setAttribute("foodItems", foodItems);
+		
+		//Redirect to a different page(view)
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("show-food.jsp");
+		requestDispatcher.forward(request, response);
+		
+		
 	}
 
 }
